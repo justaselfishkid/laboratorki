61ba58f9920e097ecf6012066f1a42f133d965cb
justaselfishkid <rustamsaliev3313@gmail.com>
2026-03-21 11:24:34 +1000

    Delete watar.py

--- watar.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        max_water = 0
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                area = (j - i) * min(height[i], height[j])
-                max_water = max(max_water, area)
-        return max_water
