79acadfd42c82b6b7811c0ac9300bd8ce12fc3b1
justaselfishkid <rustamsaliev3313@gmail.com>
2026-04-25 14:50:50 +1000

    Update watar.py

--- lab1/watar.py
+++ lab1/watar.py
@@ -6,3 +6,25 @@ class Solution:
                 area = (j - i) * min(height[i], height[j])
                 max_water = max(max_water, area)
         return max_water
+
+
+"""class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+        
+        while left < right:
+            # Вычисляем текущую площадь
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+            max_water = max(max_water, area)
+            
+            # Двигаем указатель с меньшей высотой
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_water
